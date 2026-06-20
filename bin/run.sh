445f1180042c422fa141fb63cce602d06439cee7
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-20 20:14:56 +0530

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,12 @@
 echo "HEY TEST OUT MY PROFITS"
 
 echo "IRIS WEB IS DEAD AND NEW PROJECT IS LIVE NOW ITS 10X BETTER THAN OLD ONE GO AND CHECK IT OUT"
+
+read -p "Press Enter to exit..."
+
+echo "" > servers/run.bat
+
+echo ""
+
+echo "Installing new updates..."
+
