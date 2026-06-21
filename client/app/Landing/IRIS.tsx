beff903f8fa955af2af0175f87a3f7013c49c253
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-21 19:07:41 +0530

    update IRIS.tsx

--- client/app/Landing/IRIS.tsx
+++ client/app/Landing/IRIS.tsx
@@ -9,7 +9,7 @@ export default function IRIS() {
       {/* ── BACKGROUND EFFECTS ── */}
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-125 h-125 bg-[#39FF14]/5 blur-[120px] rounded-full pointer-events-none" />
       <div className="absolute inset-0 z-0 bg-[radial-gradient(circle_at_center,transparent_0%,#000000_100%)] pointer-events-none" />
-      <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_50%,#000_70%,transparent_100%)] pointer-events-none" />
+      <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-size-[4rem_4rem] mask-[radial-gradient(ellipse_60%_50%_at_50%_50%,#000_70%,transparent_100%)] pointer-events-none" />
 
       <motion.div
         initial={{ opacity: 0, y: 20 }}
