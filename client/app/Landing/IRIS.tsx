51f5560f7549b096eacfee9425c59fddf22a5b09
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-21 19:27:01 +0530

    update IRIS.tsx

--- client/app/Landing/IRIS.tsx
+++ client/app/Landing/IRIS.tsx
@@ -14,7 +14,7 @@ export default function DeprecatedSite() {
   if (!mounted) return null;
 
   return (
-    <div className="min-h-screen bg-[#000000] text-zinc-100 flex flex-col items-center justify-center relative overflow-hidden font-sans px-6 selection:bg-[#39FF14] selection:text-black font-manrope">
+    <div className="min-h-screen bg-[#000000] text-zinc-100 flex flex-col items-center justify-center relative overflow-hidden ns px-6 selection:bg-[#39FF14] selection:text-black font-manrope">
       
       {/* --- BACKGROUND EFFECTS --- */}
       <div className="absolute inset-0 z-0 bg-[radial-gradient(circle_at_center,transparent_0%,#000000_100%)] pointer-events-none" />
