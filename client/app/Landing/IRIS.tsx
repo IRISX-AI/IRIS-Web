b31513cc8ee9182b0a717ba36e33fe1d0334485b
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-29 07:28:07 +0530

    update IRIS.tsx

--- client/app/Landing/IRIS.tsx
+++ client/app/Landing/IRIS.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { ArrowRight, Zap, Lock, Sparkles, Cpu, Shield } from "lucide-react";
+import { ArrowRight} from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function DeprecatedSite() {
